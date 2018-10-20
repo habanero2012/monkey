a9989e34effdfbcd11d0084b369e4cd298253224
hsato <sato1101@live.jp>
2018-10-20 23:07:56 +0900

    add EvalBooleanExpression

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -35,6 +35,38 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+		//{"1 < 2", true},
+		//{"1 > 2", false},
+		//{"1 < 1", false},
+		//{"1 > 1", false},
+		//{"1 == 1", true},
+		//{"1 != 1", false},
+		//{"1 == 2", false},
+		//{"1 != 2", true},
+		//{"true == true", true},
+		//{"false == false", true},
+		//{"true == false", false},
+		//{"true != false", true},
+		//{"false != true", true},
+		//{"(1 < 2) == true", true},
+		//{"(1 < 2) == false", false},
+		//{"(1 > 2) == true", false},
+		//{"(1 > 2) == false", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
@@ -55,5 +87,19 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 		return false
 	}
 
+	return true
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t",
+			result.Value, expected)
+		return false
+	}
 	return true
 }
